2bd2ea13d3bb88d62131d9b50d8204f06dd4d9df
GuilhermeEsdras <guilherme.esdras@outlook.com>
2020-04-30 19:38:55 -0300

    Atualizando :sparkles:

--- em Go/A Linguagem Go/01 - O Básico/08 - Ponteiros/Go by Example/pointers.go
+++ em Go/A Linguagem Go/01 - O Básico/08 - Ponteiros/Go by Example/pointers.go
@@ -1,29 +1,26 @@
 /*
   O Go suporta ponteiros , permitindo que você passe referências a valores e registros dentro do seu programa.
-
   Mostraremos como os ponteiros funcionam em contraste com os valores com 2 funções: zeroval e zeroptr.
-  zeroval tem um parâmetro inteiro, então os argumentos serão passados ​​a ele por valor.
-  zeroval obterá uma cópia de ival distinta daquela na função de chamada.
-
-  zeroptr em contraste, tem um parâmetro *int, o que significa que é necessário um ponteiro int.
-  O código *iptr no corpo da função desreferencia o ponteiro do endereço de memória para o valor atual nesse
-  endereço. Atribuir um valor a um ponteiro não referenciado altera o valor no endereço referenciado.
-
-  A sintaxe &i fornece o endereço de memória de i, ou seja, um ponteiro para i.
-
-  Os ponteiros também podem ser impressos.
-
-  zeroval não altera a variável i no main, mas zeroptr o faz porque tem uma referência para o endereço de memória para essa variável.
 */
 
 package main
 
 import "fmt"
 
+/*
+  zeroval tem um parâmetro inteiro, então os argumentos serão passados ​​a ele por valor.
+  zeroval obterá uma cópia de ival distinta daquela na função de chamada.
+*/
 func zeroval(ival int) {
 	ival = 0
 }
 
+/*
+  zeroptr em contraste, tem um parâmetro *int, o que significa que é necessário um ponteiro int.
+  O código *iptr no corpo da função desreferencia o ponteiro do endereço de memória para o valor atual nesse
+  endereço.
+  Atribuir um valor a um ponteiro não referenciado altera o valor no endereço referenciado.
+*/
 func zeroptr(iptr *int) {
 	*iptr = 0
 }
@@ -35,10 +32,12 @@ func main() {
 	zeroval(i)
 	fmt.Println("zeroval:", i)
 
-	zeroptr(&i)
+	zeroptr(&i) // A sintaxe &i fornece o endereço de memória de i, ou seja, um ponteiro para i.
 	fmt.Println("zeroptr:", i)
 
-	fmt.Println("pointer:", &i)
+	fmt.Println("pointer:", &i) // Os ponteiros também podem ser impressos.
+
+	// zeroval não altera a variável i no main, mas zeroptr o faz porque tem uma referência para o endereço de memória para essa variável.
 }
 
 /*
